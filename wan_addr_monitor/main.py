75b978cd70c27d4925abe90f0ad1fea21a6cb699
mark <89848850+wottreng@users.noreply.github.com>
2021-12-30 09:26:33 -0500

    Update main.py

--- wan_addr_monitor/main.py
+++ wan_addr_monitor/main.py
@@ -73,12 +73,10 @@ def record_current_time():
         doc.write(str(time.time()).split(".")[0])
 
 if __name__ == '__main__':
-    while 1:
-        change_working_dir()
-        record_current_time()
-        current_wan_ipv4_addr = get_current_wan_addr()
-        result = compare_wan_addr_to_record_addr(current_wan_ipv4_addr)
-        if result == False:
-            # update dns ipv4
-            update_dns_addr_on_record(current_wan_ipv4_addr)
-        time.sleep(300)
+    change_working_dir()
+    record_current_time()
+    current_wan_ipv4_addr = get_current_wan_addr()
+    result = compare_wan_addr_to_record_addr(current_wan_ipv4_addr)
+    if result == False:
+        # update dns ipv4
+        update_dns_addr_on_record(current_wan_ipv4_addr)
